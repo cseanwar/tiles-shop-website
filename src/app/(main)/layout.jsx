91469eda48d17c172000e0f3264ac95ae0722fdf
Anwar Hossain <cseanwar@gmail.com>
2026-04-29 23:58:18 +0200

    Added Banner with Swiper slider

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,3 +1,4 @@
+import Banner from "@/components/shared/Banner";
 import Navbar from "@/components/shared/Navbar";
 import React from "react";
 
@@ -5,6 +6,7 @@ const MainLayout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <Banner />
       {children}
     </>
   );
---
+++ src/components/shared/Banner.jsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from 'swiper/modules';
+import banner1 from "@/assets/banner-1.webp";
+import banner2 from "@/assets/banner-2.webp";
+import banner3 from "@/assets/banner-3.webp";
+import banner4 from "@/assets/banner-4.webp";
+import Image from "next/image";
+
+import "swiper/swiper-bundle.css";
+import Link from "next/link";
+
+
+const Banner = () => {
+  return (
+    <div className="mb-10">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation={true}
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination]}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className="relative">
+            <Image
+              src={banner1}
+              alt="Living room tiles"
+              width={800}
+              height={400}
+              className="w-full"
+            />
+            <div className="absolute bottom-60 left-0 right-0 text-center space-y-5">
+              <h2 className="text-7xl text-white leading-23">
+                Shaping Dreams <br /> Into Living Spaces
+              </h2>
+              <p className="text-base text-white">
+                Set the tone of your space with tiles that go beyond function
+              </p>
+              <Link href={"/all-tiles"}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-sm cursor-pointer">
+                  Browse Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <Image
+              src={banner2}
+              alt="Outdoor tiles"
+              width={800}
+              height={400}
+              className="w-full"
+            />
+            <div className="absolute bottom-55 left-0 right-0 text-center space-y-5">
+              <h2 className="text-7xl text-white leading-23">
+                Beauty That Lasts <br /> Beyond Seasons
+              </h2>
+              <p className="text-base text-white">
+                Build to endure, designed to captivate. Our tiles are crafted to
+                withstand <br /> the test of time, ensuring your space remains
+                stunning for years to come.
+              </p>
+              <Link href={"/all-tiles"}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-sm cursor-pointer">
+                  Browse Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <Image
+              src={banner3}
+              alt="Kitchen tiles"
+              width={800}
+              height={400}
+              className="w-full"
+            />
+            <div className="absolute bottom-55 left-0 right-0 text-center space-y-5">
+              <h2 className="text-7xl text-white leading-23">
+                Where Tile Meets <br /> Culinary Craft
+              </h2>
+              <p className="text-base text-white">
+                Elevate your kitchen with our exquisite tiles, designed to inspire culinary creativity <br /> and transform your cooking space into a masterpiece of style and functionality.
+              </p>
+              <Link href={"/all-tiles"}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-sm cursor-pointer">
+                  Browse Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <Image
+            src={banner4}
+            alt="Bathroom tiles"
+            width={800}
+            height={400}
+            className="w-full"
+          />
+          <div className="absolute bottom-60 left-0 right-0 text-center space-y-5">
+              <h2 className="text-7xl text-white leading-23">
+                Timeless Tiles for <br /> Tranquil Spaces
+              </h2>
+              <p className="text-base text-white">
+                Elevate your bathroom with our timeless tiles, designed to create
+              </p>
+              <Link href={"/all-tiles"}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-sm cursor-pointer">
+                  Browse Now
+                </button>
+              </Link>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Banner;
